a0d29edc07413101db912a5229457201919021a4
André Sobral <andre.g.sobral@gmail.com>
2016-03-15 14:50:08 +0000

    tipo real, realmente bem

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -22,7 +22,7 @@ void zu::postfix_writer::do_integer_node(cdk::integer_node * const node, int lvl
 }
 
 void zu::postfix_writer::do_double_node(cdk::double_node * const node, int lvl) {
-  _pf.DOUBLE(node->value());
+	/* TODO */
 }
 
 void zu::postfix_writer::do_string_node(cdk::string_node * const node, int lvl) {
