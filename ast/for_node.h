6f18ca859dfd0701ac8dcf1c8721d963f1d8beb3
André Sobral <andre.g.sobral@gmail.com>
2016-03-15 19:44:46 +0000

    readme update

--- ast/for_node.h
+++ ast/for_node.h
@@ -1,5 +1,5 @@
-#ifndef __CDK_FORNODE_H__
-#define __CDK_FORNODE_H__
+#ifndef __FORNODE_H__
+#define __FORNODE_H__
 
 #include <cdk/ast/expression_node.h>
 
@@ -9,22 +9,22 @@ namespace zu {
    * Class for describing while-cycle nodes.
    */
   class for_node: public cdk::basic_node {
-    cdk::expression_node *_init, *_test, *_step;
+    cdk::sequence_node *_init, *_test, *_step;
     cdk::basic_node *_block;
 
   public:
-    inline for_node(int lineno, cdk::expression_node *init,cdk::expression_node *test,cdk::expression_node *step, cdk::basic_node *block) :
+    inline for_node(int lineno, cdk::sequence_node *init,cdk::sequence_node *test,cdk::sequence_node *step, cdk::basic_node *block) :
         basic_node(lineno), _init(init),_test(test),_step(step), _block(block) {
     }
 
   public:
-    inline cdk::expression_node *init() {
+    inline cdk::sequence_node *init() {
       return _init;
     }
-    inline cdk::expression_node *test() {
+    inline cdk::sequence_node *test() {
       return _test;
     }
-    inline cdk::expression_node *step() {
+    inline cdk::sequence_node *step() {
       return _step;
     }
     inline cdk::basic_node *block() {
