67a504d1fc4777e253c1b662e14918f8309d4da7
3ximus <fabio_r11@hotmail.com>
2016-04-15 15:20:27 +0100

    unary expression

--- targets/xml_writer.cpp
+++ targets/xml_writer.cpp
@@ -40,10 +40,18 @@ void zu::xml_writer::do_neg_node(cdk::neg_node * const node, int lvl) {
   processUnaryExpression(node, lvl);
 }
 
-void zu::xml_writer::do_simetry_node(zu::simetry_node * const node, int lvl) {/* TODO */}
-void zu::xml_writer::do_not_node(zu::not_node * const node, int lvl) {/* TODO */}
-void zu::xml_writer::do_identity_node(zu::identity_node * const node, int lvl) {/* TODO */}
-void zu::xml_writer::do_position_node(zu::position_node * const node, int lvl) {/* TODO */}
+void zu::xml_writer::do_simetry_node(zu::simetry_node * const node, int lvl) {
+  processUnaryExpression(node, lvl);
+}
+void zu::xml_writer::do_not_node(zu::not_node * const node, int lvl) {
+  processUnaryExpression(node, lvl);
+}
+void zu::xml_writer::do_identity_node(zu::identity_node * const node, int lvl) {
+  processUnaryExpression(node, lvl);
+}
+void zu::xml_writer::do_position_node(zu::position_node * const node, int lvl) {
+  processUnaryExpression(node, lvl);
+}
 
 //---------------------------------------------------------------------------
 
