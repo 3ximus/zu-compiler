38e8e9b639814ac6a4ab16c5a045ed4770560b55
eximus <fabio_r11@hotmail.com>
2016-05-18 15:50:09 +0100

    read node

--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -112,8 +112,23 @@ void zu::type_checker::do_variable_node(zu::variable_node * const node, int lvl)
 	node->type(node->zu_type());
 }
 
-
 //---------------------------------------------------------------------------
+
+inline bool zu::type_checker::processBinaryRead(cdk::binary_expression_node * const node, int lvl) {
+	if (node->right()->name() == "read_node" && node->left()->name() == "read_node") {
+		throw std::string("two binary expression arguments cant be read");
+	else if (node->left()->name() == "read_node" && node->right->type()->name() == basic_type::TYPE_INT || node->right()->type()->name() == basic_type::TYPE_DOUBLE) {
+		node->left()->type(node->right()->type());
+		node->type(node->right()->type());
+		return true;
+	}
+	else if (node->right()->name() == "read_node" && node->left->type()->name() == basic_type::TYPE_INT || node->left()->type()->name() == basic_type::TYPE_DOUBLE) {
+		node->right()->type(node->left()->type());
+		node->type(node->left()->type());
+		return true;
+	}
+	return false;
+}
 
 inline void zu::type_checker::processBinaryExpression(cdk::binary_expression_node * const node, int lvl) {
 	ASSERT_UNSPEC;
@@ -139,6 +154,7 @@ inline void zu::type_checker::processNotPointer(cdk::binary_expression_node * co
 
 void zu::type_checker::do_add_node(cdk::add_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	/* if one of the operands is a pointer the other must be an integer to calculate offset */
 	if ((node->left()->type()->name() == basic_type::TYPE_POINTER && node->right()->type()->name() == basic_type::TYPE_INT) || (node->right()->type()->name() == basic_type::TYPE_POINTER && node->left()->type()->name() == basic_type::TYPE_INT))  {
 		node->type(new basic_type(4, basic_type::TYPE_POINTER));
@@ -157,6 +173,7 @@ void zu::type_checker::do_add_node(cdk::add_node * const node, int lvl) {
 
 void zu::type_checker::do_sub_node(cdk::sub_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	/* if both operands are pointer the type is int represententing distance between them */
 	if (node->left()->type()->name() == basic_type::TYPE_POINTER && node->right()->type()->name() == basic_type::TYPE_POINTER) {
 		if (node->left()->type()->subtype() == node->right()->type()->subtype())
@@ -176,6 +193,7 @@ void zu::type_checker::do_sub_node(cdk::sub_node * const node, int lvl) {
 
 void zu::type_checker::do_mul_node(cdk::mul_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processNotPointer(node);
 
 	if(node->left()->type()->name() == basic_type::TYPE_DOUBLE || node->right()->type()->name() == basic_type::TYPE_DOUBLE) {
@@ -188,6 +206,7 @@ void zu::type_checker::do_mul_node(cdk::mul_node * const node, int lvl) {
 
 void zu::type_checker::do_div_node(cdk::div_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processNotPointer(node);
 
 	if(node->left()->type()->name() == basic_type::TYPE_DOUBLE || node->right()->type()->name() == basic_type::TYPE_DOUBLE) {
@@ -200,6 +219,7 @@ void zu::type_checker::do_div_node(cdk::div_node * const node, int lvl) {
 
 void zu::type_checker::do_mod_node(cdk::mod_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processNotPointer(node);
 
 	if (node->left()->type()->name() == basic_type::TYPE_DOUBLE && node->right()->type()->name() == basic_type::TYPE_DOUBLE)
@@ -210,6 +230,7 @@ void zu::type_checker::do_mod_node(cdk::mod_node * const node, int lvl) {
 
 void zu::type_checker::do_lt_node(cdk::lt_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processNotPointer(node);
 	processSameTypes(node);
 
@@ -218,6 +239,7 @@ void zu::type_checker::do_lt_node(cdk::lt_node * const node, int lvl) {
 
 void zu::type_checker::do_le_node(cdk::le_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processNotPointer(node);
 	processSameTypes(node);
 
@@ -226,6 +248,7 @@ void zu::type_checker::do_le_node(cdk::le_node * const node, int lvl) {
 
 void zu::type_checker::do_ge_node(cdk::ge_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processNotPointer(node);
 	processSameTypes(node);
 
@@ -234,6 +257,7 @@ void zu::type_checker::do_ge_node(cdk::ge_node * const node, int lvl) {
 
 void zu::type_checker::do_gt_node(cdk::gt_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processNotPointer(node);
 	processSameTypes(node);
 
@@ -244,6 +268,7 @@ void zu::type_checker::do_gt_node(cdk::gt_node * const node, int lvl) {
 
 void zu::type_checker::do_ne_node(cdk::ne_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processSameTypes(node);
 
 	node->type(new basic_type(4, basic_type::TYPE_INT));
@@ -251,6 +276,7 @@ void zu::type_checker::do_ne_node(cdk::ne_node * const node, int lvl) {
 
 void zu::type_checker::do_eq_node(cdk::eq_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	processSameTypes(node);
 
 	node->type(new basic_type(4, basic_type::TYPE_INT));
@@ -258,6 +284,7 @@ void zu::type_checker::do_eq_node(cdk::eq_node * const node, int lvl) {
 
 void zu::type_checker::do_and_node(zu::and_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	if(node->left()->type()->name() != basic_type::TYPE_INT || node->right()->type()->name() != basic_type::TYPE_INT)
 		throw std::string("wrong types in arguments of binary expression");
 
@@ -266,6 +293,7 @@ void zu::type_checker::do_and_node(zu::and_node * const node, int lvl) {
 
 void zu::type_checker::do_or_node(zu::or_node * const node, int lvl) {
 	processBinaryExpression(node, lvl);
+	if (processBinaryRead(node, lvl)) return;
 	if(node->left()->type()->name() != basic_type::TYPE_INT || node->right()->type()->name() != basic_type::TYPE_INT)
 		throw std::string("wrong types in arguments of binary expression");
 
@@ -329,6 +357,20 @@ void zu::type_checker::do_assignment_node(zu::assignment_node * const node, int 
 
 	node->lvalue()->accept(this, lvl + 2);
 	node->rvalue()->accept(this, lvl + 2);
+
+	if (node->rvalue()->name() == "read_node" && node->lvalue()->name() == "read_node") {
+		throw std::string("two binary expression arguments cant be read");
+	else if (node->lvalue()->name() == "read_node" && node->rvalue->type()->name() == basic_type::TYPE_INT || node->rvalue()->type()->name() == basic_type::TYPE_DOUBLE) {
+		node->lvalue()->type(node->rvalue()->type());
+		node->type(node->rvalue()->type());
+		return;
+	}
+	else if (node->rvalue()->name() == "read_node" && node->lvalue->type()->name() == basic_type::TYPE_INT || node->lvalue()->type()->name() == basic_type::TYPE_DOUBLE) {
+		node->rvalue()->type(node->lvalue()->type());
+		node->type(node->lvalue()->type());
+		return;
+	}
+
 	if ((node->lvalue()->type()->name() != basic_type::TYPE_DOUBLE && node->rvalue()->type()->name() != basic_type::TYPE_INT) || node->lvalue()->type()->name() != node->rvalue()->type()->name())
 	  throw std::string("wrong type in left argument of assignment expression");
 
@@ -350,7 +392,11 @@ void zu::type_checker::do_print_node(zu::print_node * const node, int lvl) {
 //---------------------------------------------------------------------------
 
 void zu::type_checker::do_read_node(zu::read_node * const node, int lvl) {
-	/* TODO */
+
+	/* TODO
+	 * Currently being processed in processBinaryRead and assigment node
+	 */
+
 	//node->argument()->accept(this, lvl + 2);
 }
 
