73d95290c179eaf03de0af70e59ee40e38bbedba
Andre Sobral <andre.sobral@tecnico.ulisboa.pt>
2016-05-23 00:04:53 +0100

    teste pratico trainning

---
+++ ast/apply_node.h
@@ -0,0 +1,30 @@
+#ifndef __ZU_APPLY_NODE_H__
+#define __ZU_APPLY_NODE_H__
+
+#include <string>
+
+namespace zu {
+
+  /**
+   * Class APPLnode
+   */
+  class apply_node: public cdk::expression_node {
+  protected:
+	zu::function_call_node _func;
+
+  public:
+    inline apply_node(int lineno, zu::function_call_node func) :
+        cdk::expression_node(lineno), _func(s) {
+    }
+
+  public:
+
+    void accept(basic_ast_visitor *sp, int level) {
+      sp->do_apply_node(this, level);
+    }
+
+  };
+
+} // zu
+
+#endif
